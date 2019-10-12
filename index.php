df28c603a9b3992dfa9a1d6681793e28767cea7f
imbdev <moose@inspiredmommyboss.com>
2019-10-12 12:25:27 +0800

    Final consolidated version - 12 Oct 2019

---
+++ index.php
@@ -0,0 +1,42 @@
+<?php
+/*
+Plugin Name: CG Property
+Plugin URI: https://cyberizegroup.com/
+Description: Displays Property Category List, Google Map etc. via Shortcodes
+Version: 1.0
+Author: Cyberize Group
+Author URI: https://cyberizegroup.com/
+License: GPLv2 or later
+Text Domain: cyberizeframework
+*/
+
+// If this file is called directly, abort.
+if ( ! defined( 'WPINC' ) ) {
+  die;
+}
+
+define( 'CGPLUGIN_URL', plugin_dir_url( __FILE__ ) );
+
+// Enqueue Plugin CSS
+include( plugin_dir_path( __FILE__ ) . 'includes/property-shortcodes-styles.php');
+
+// Enqueue Plugin JavaScript
+// include( plugin_dir_path( __FILE__ ) . 'includes/property-shortcodes-scripts.php');
+
+// Create Properties Custom Post Type with Taxonomies (property-type & listing-status)
+include( plugin_dir_path( __FILE__ ) . 'includes/properties-cpt.php');
+
+// Create Single Property ACF Fields
+include( plugin_dir_path( __FILE__ ) . 'includes/property-acf.php');
+
+// Create Plugin Admin Menus and Setting Pages
+include( plugin_dir_path( __FILE__ ) . 'includes/menu-page.php');
+
+// Create Google Map with Property List Shortcode
+include( plugin_dir_path( __FILE__ ) . 'includes/property-list.php');
+
+// Create Google Map with Property Google Map List
+include( plugin_dir_path( __FILE__ ) . 'includes/property-gmap-list.php');
+
+// Create Google Map with Property Categories
+include( plugin_dir_path( __FILE__ ) . 'includes/property-gmap-category.php');
